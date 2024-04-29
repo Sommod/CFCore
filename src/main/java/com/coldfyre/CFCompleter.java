5727f2ffc647f3e0c991dfaf3e711cc819fda127
Sommod <>
2024-04-29 00:26:56 -0400

    Added Plugin classes

---
+++ src/main/java/com/coldfyre/CFCompleter.java
@@ -0,0 +1,16 @@
+package com.coldfyre;
+
+import java.util.List;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+public class CFCompleter implements TabCompleter {
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+		return null;
+	}
+
+}
---
+++ src/main/java/com/coldfyre/CFCore.java
@@ -0,0 +1,7 @@
+package com.coldfyre;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CFCore extends JavaPlugin {
+
+}
