601747f14a6f080457f1c087e2fc2cc8d850b46b
BuildTools <unconfigured@null.spigotmc.org>
2024-05-10 00:40:52 -0400

    generic push

--- src/main/java/com/coldfyre/CFCore.java
+++ src/main/java/com/coldfyre/CFCore.java
@@ -3,5 +3,14 @@ package com.coldfyre;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CFCore extends JavaPlugin {
-
+	
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
 }
